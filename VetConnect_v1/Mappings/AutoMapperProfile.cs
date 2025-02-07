e873e3870ee884c5d3bdb0698e868d8abcacc2b5
RommelCanepa <rommelcanepa01@gmail.com>
2025-02-07 01:45:32 -0600

    Add files via upload

---
+++ VetConnect_v1/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using VetConnect_v1.Models;
+using VetConnect_v1.DTOs;
+
+
+namespace VetConnect_v1.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Cita, CitaDto>().ReverseMap()
+            .ForMember(dest => dest.FechaHora, opt => opt.MapFrom(src => src.FechaHora));
+            CreateMap<Usuario, UsuarioDto>().ReverseMap();
+            CreateMap<Mascota, MascotaDto>().ReverseMap();
+            // Mapeo para la búsqueda de veterinarios (DTO simple)
+            CreateMap<Veterinario, VeterinarioDto>();
+           // Agrega más mapeos aquí si tienes otros DTOs
+        }
+    }
+}
